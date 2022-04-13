4a8c431a75371bd9451f7d16282518c020ae09b5
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-04-13 11:22:47 +0200

    Arguments - Adding - Implementing -h and bad arguments gestion

--- src/init_ftp.c
+++ src/init_ftp.c
@@ -8,9 +8,31 @@
 #include "macro.h"
 #include "init_ftp.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void display_help(data_t *head)
+{
+    printf("My FTP Usage\n");
+    destroy_data(head);
+}
+
+void display_error(char *str, data_t *head)
+{
+    fprintf(stderr, "%s\n", str);
+    destroy_data(head);
+}
 
 int argument_gestion(int ac, char **av, data_t *head)
 {
+    if (ac == 2 && strcmp(av[1], "-h") == 0) {
+        display_help(head);
+        return ARGUMENT_ERROR;
+    }
+    if (ac != 3) {
+        display_error("Invalid number of arguments.", head);
+        return ARGUMENT_ERROR;
+    }
     return FUNCTION_SUCCESS;
 }
 
@@ -22,7 +44,6 @@ int init_ftp(int ac, char **av)
         return FTP_ERROR;
     if (argument_gestion(ac, av, head) == ARGUMENT_ERROR)
         return ARGUMENT_ERROR;
-
     destroy_data(head);
     return PROGRAM_SUCCESS;
 }
